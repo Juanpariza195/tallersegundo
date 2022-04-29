1041346d72da419cd5caeb5a2a33905c55e8e21e
Juanpariza195 <79613679+Juanpariza195@users.noreply.github.com>
2022-04-29 02:45:26 -0500

    Add files via upload

---
+++ modelo tres/primer_commit_diabetes.py
@@ -0,0 +1,23 @@
+import numpy as np
+import pandas as pd
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+from warnings import simplefilter
+from sklearn.tree import DecisionTreeClassifier
+
+simplefilter(action='ignore', category=FutureWarning)
+
+url = 'Diabetes.csv'
+data = pd.read_csv(url)
+
+# Tratamiento de la data
+
+data.Sex.replace(['female', 'male'], [0, 1], inplace=True)
+data['Embarked'].replace(['S', 'C', 'Q'], [0, 1, 2], inplace=True)
+data.drop(['Cabin', 'PassengerId', 'Name', 'Ticket'], axis= 1, inplace = True)
+data.Age.replace(np.nan, 30, inplace=True)
+rangos = [0, 8, 15, 18, 25, 40, 60, 100]
+nombres = ['1', '2', '3', '4', '5', '6', '7']
+data.Age = pd.cut(data.Age, rangos, labels=nombres)
+data.dropna(axis=0,how='any', inplace=True)
